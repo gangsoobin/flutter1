f44e6f087249f5fe7dfaa8be7017bf9c53f8f515
gangsoobin <69720684+gangsoobin@users.noreply.github.com>
2020-08-17 17:05:24 +0900

    Update Comment

--- main.dart
+++ main.dart
@@ -5,32 +5,46 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); // app execute // https://software-creator.tistory.com/10
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatelessWidget { // default stable widget
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { // draw UI
     //final wordPair = WordPair.random(); // wordPair from english_words
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),/*
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+    return MaterialApp( // make app using MaterialApp what google's default design
+      title: '001261 app',
+      home: RandomWords()/*
+      home: Scaffold( // default material layout
+        appBar: AppBar( // for make app layout
+          title: Text('Welcome to Flutter'), // app title
         ),
-        body: Center(
-          child: RandomWords(),
+        body: Center( // align widget
+          child: RandomWords(), // call RandomWords class
+          //child: Text(findQuotient())
           //child: Text(wordPair.asPascalCase), // asPascalCase like UpperCamelCase
         ),
       ),*/
     );
   }
 }
+/*
+String findQuotient() {
+  int value = 5~/3;
+  return value.toString();
+}*/
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState(); // create RandomWordsState class
+}
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
+  final _suggestions = <WordPair>[]; // array, WordPair data type
+  // final can't modify, delete, but can add element
+  // const can't modify, delete, add
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  //final wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,35 +54,28 @@ class RandomWordsState extends State<RandomWords> {
       body: _buildSuggestions(),
     );
   }
-  final wordPair = WordPair.random();
 
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
+        itemBuilder: (context, i) { // itemBuilder called when scrolled // https://agoalofglory.github.io/2019/07/20/2019/20190720_01_Flutter_First_App/
+          if (i.isOdd) return Divider(); // if i is odd, return line
 
-          final index = i ~/ 2;
+          final index = i ~/ 2; // for sync with suggestion
           if(index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
+            _suggestions.addAll(generateWordPairs().take(10)); // take WordPairs 10 each every times
           }
           return _buildRow(_suggestions[index]);
         }
     );
   }
   Widget _buildRow(WordPair pair) {
-    return ListTile(
+    return ListTile( // https://medium.com/@suragch/a-complete-guide-to-flutters-listtile-597a20a3d449
       title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+        pair.asPascalCase, // string, get WordPair and UpperCamel
+        style: _biggerFont, // set style
       ),
     );
   }
   //return Text(wordPair.asPascalCase);
-}
-
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
 }
