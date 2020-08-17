9fe9ed7cf0d75c4f0973c50ee3a85a7b244a1726
gangsoobin <69720684+gangsoobin@users.noreply.github.com>
2020-08-17 10:01:22 +0900

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,74 @@
+//Copyright 2018 The Flutter team. All rights reserved.
+//Use of this source code is governed by a BSD-style license that can be
+//found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random(); // wordPair from english_words
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),/*
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: RandomWords(),
+          //child: Text(wordPair.asPascalCase), // asPascalCase like UpperCamelCase
+        ),
+      ),*/
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  final wordPair = WordPair.random();
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if(index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+  //return Text(wordPair.asPascalCase);
+}
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
